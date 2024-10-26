6deb219bf903b374f7414ea110bbaf3243895744
LX Null <the-person@lxnull.com>
2024-10-26 23:51:26 +0100

    reinstate trailing slash redirects

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -10,7 +10,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lxnull.com',
-  trailingSlash: 'never',
   integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
